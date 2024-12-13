02cb41a8b8d60f4d4df7881a7da547b8146b5cdb
n.kalgushkin <rootacces00@gmail.com>
2024-12-13 15:44:53 +0100

    init commit

--- main.ts
+++ main.ts
@@ -1,134 +1,235 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
+import { pinyin } from "pinyin-pro";
+import { EditorView, Decoration, DecorationSet, ViewPlugin, ViewUpdate } from "@codemirror/view";
+import { RangeSetBuilder } from "@codemirror/state";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface ToneColorSettings {
+    tone1: string;
+    tone2: string;
+    tone3: string;
+    tone4: string;
+    tone0: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: ToneColorSettings = {
+    tone1: "blue",
+    tone2: "green",
+    tone3: "black",
+    tone4: "red",
+    tone0: "gray"
+};
+
+export default class ChineseColorEditorPlugin extends Plugin {
+    settings: ToneColorSettings;
+
+    async onload() {
+        console.log("Loading Chinese Color Editor Plugin");
+        await this.loadSettings();
+        this.addSettingTab(new ChineseColorEditorSettingTab(this.app, this));
+
+        // Register the editor extension
+        this.registerEditorExtension(chineseColorViewPlugin(this.settings));
+    }
+
+    onunload() {
+        console.log("Unloading Chinese Color Editor Plugin");
+    }
+
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class ChineseColorEditorSettingTab extends PluginSettingTab {
+    plugin: ChineseColorEditorPlugin;
 
-	async onload() {
-		await this.loadSettings();
+    constructor(app: App, plugin: ChineseColorEditorPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    display(): void {
+        const { containerEl } = this;
+        containerEl.empty();
+        containerEl.createEl("h2", { text: "Chinese Color Editor Settings" });
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        new Setting(containerEl)
+            .setName("Tone 1 Color")
+            .setDesc("Color for first tone")
+            .addText((text) =>
+                text
+                    .setValue(this.plugin.settings.tone1)
+                    .onChange(async (value) => {
+                        this.plugin.settings.tone1 = value;
+                        await this.plugin.saveSettings();
+                    })
+            );
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        new Setting(containerEl)
+            .setName("Tone 2 Color")
+            .setDesc("Color for second tone")
+            .addText((text) =>
+                text
+                    .setValue(this.plugin.settings.tone2)
+                    .onChange(async (value) => {
+                        this.plugin.settings.tone2 = value;
+                        await this.plugin.saveSettings();
+                    })
+            );
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        new Setting(containerEl)
+            .setName("Tone 3 Color")
+            .setDesc("Color for third tone")
+            .addText((text) =>
+                text
+                    .setValue(this.plugin.settings.tone3)
+                    .onChange(async (value) => {
+                        this.plugin.settings.tone3 = value;
+                        await this.plugin.saveSettings();
+                    })
+            );
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+        new Setting(containerEl)
+            .setName("Tone 4 Color")
+            .setDesc("Color for fourth tone")
+            .addText((text) =>
+                text
+                    .setValue(this.plugin.settings.tone4)
+                    .onChange(async (value) => {
+                        this.plugin.settings.tone4 = value;
+                        await this.plugin.saveSettings();
+                    })
+            );
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+        new Setting(containerEl)
+            .setName("Neutral Tone Color")
+            .setDesc("Color for neutral/no tone")
+            .addText((text) =>
+                text
+                    .setValue(this.plugin.settings.tone0)
+                    .onChange(async (value) => {
+                        this.plugin.settings.tone0 = value;
+                        await this.plugin.saveSettings();
+                    })
+            );
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+function chineseColorViewPlugin(settings: ToneColorSettings) {
+    return ViewPlugin.fromClass(class {
+        decorations: DecorationSet;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+        constructor(view: EditorView) {
+            this.decorations = this.computeDecorations(view, settings);
+        }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+        update(update: ViewUpdate) {
+            if (update.docChanged || update.viewportChanged) {
+                this.decorations = this.computeDecorations(update.view, settings);
+            }
+        }
+
+        computeDecorations(view: EditorView, settings: ToneColorSettings): DecorationSet {
+            const builder = new RangeSetBuilder<Decoration>();
+            const text = view.state.doc.toString();
+            // Only consider the visible viewport for performance
+            const { from, to } = view.viewport;
+            
+            for (let pos = from; pos < to; ) {
+                const char = text[pos];
+                const charSize = char.codePointAt(0)! > 0xffff ? 2 : 1; // Handle surrogate pairs if needed
+                if (isChineseChar(char)) {
+                    const pyArr = pinyin(char, { toneType: 'num', type: 'array' });
+                    const numericPinyin = pyArr[0] || "";
+                    const { accented, color } = getAccentedColoredPinyin(numericPinyin, settings);
+
+                    const deco = Decoration.mark({
+                        attributes: {
+                            style: `color:${color};`,
+                            title: accented
+                        }
+                    });
+                    builder.add(pos, pos + charSize, deco);
+                }
+                pos += charSize;
+            }
+
+            return builder.finish();
+        }
+    }, {
+        decorations: v => v.decorations
+    });
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+function isChineseChar(char: string): boolean {
+    return /[\u4E00-\u9FFF]/.test(char);
+}
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+function getAccentedColoredPinyin(numericPinyin: string, settings: ToneColorSettings): {accented: string; color: string} {
+    const match = numericPinyin.match(/^([a-züÜ]+)(\d)?$/i);
+    if (!match) {
+        return { accented: numericPinyin, color: settings.tone0 };
+    }
 
-	display(): void {
-		const {containerEl} = this;
+    const [_, base, toneStr] = match;
+    const tone = toneStr ? parseInt(toneStr, 10) : 0;
 
-		containerEl.empty();
+    let color = settings.tone0;
+    if (tone === 1) color = settings.tone1;
+    else if (tone === 2) color = settings.tone2;
+    else if (tone === 3) color = settings.tone3;
+    else if (tone === 4) color = settings.tone4;
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    const accented = addToneMark(base, tone);
+    return { accented, color };
+}
+
+function addToneMark(syllable: string, tone: number): string {
+    if (tone < 1 || tone > 4) return syllable;
+
+    const toneMap: Record<string, string[]> = {
+        a: ["ā", "á", "ǎ", "à"],
+        e: ["ē", "é", "ě", "è"],
+        i: ["ī", "í", "ǐ", "ì"],
+        o: ["ō", "ó", "ǒ", "ò"],
+        u: ["ū", "ú", "ǔ", "ù"],
+        ü: ["ǖ", "ǘ", "ǚ", "ǜ"],
+        A: ["Ā", "Á", "Ǎ", "À"],
+        E: ["Ē", "É", "Ě", "È"],
+        I: ["Ī", "Í", "Ǐ", "Ì"],
+        O: ["Ō", "Ó", "Ǒ", "Ò"],
+        U: ["Ū", "Ú", "Ǔ", "Ù"],
+        Ü: ["Ǖ", "Ǘ", "Ǚ", "Ǜ"]
+    };
+
+    const vowelsOrder = ["a", "A", "o", "O", "e", "E"];
+    let targetIdx = -1;
+    let chosenVowel = "";
+
+    for (const v of vowelsOrder) {
+        targetIdx = syllable.indexOf(v);
+        if (targetIdx !== -1) {
+            chosenVowel = v;
+            break;
+        }
+    }
+
+    if (targetIdx === -1) {
+        const vowelMatch = syllable.match(/[iIuUüÜ]/g);
+        if (vowelMatch) {
+            const lastVowel = vowelMatch[vowelMatch.length - 1];
+            chosenVowel = lastVowel;
+            targetIdx = syllable.lastIndexOf(lastVowel);
+        }
+    }
+
+    if (targetIdx !== -1 && toneMap[chosenVowel]) {
+        return syllable.slice(0, targetIdx) + toneMap[chosenVowel][tone - 1] + syllable.slice(targetIdx + 1);
+    }
+
+    return syllable;
 }
